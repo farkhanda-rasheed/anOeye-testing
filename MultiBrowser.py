301e06f08ecf0192de6723fa4fb17c588cec1f96
farkhanda-rasheed <farkhanda.wasay@yahoo.com>
2021-06-09 12:08:46 +0500

    anOeye is opened in Chrome, Firefox and Internet explorer

---
+++ MultiBrowser.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#driver=webdriver.Chrome(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\chromedriver.exe")
+driver=webdriver.Firefox(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\geckodriver.exe")
+#driver=webdriver.Ie(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\IEDriverServer.exe")
+
+driver.get("https://anoeye.com/login")
