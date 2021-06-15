45f50f0528df5e82061e2686f14e55531f026542
farkhanda-rasheed <farkhanda.wasay@yahoo.com>
2021-06-15 14:36:16 +0500

    profile page appears and user change password successfully

--- profilecheckChangePassword.py
+++ profilecheckChangePassword.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 browser = webdriver.Firefox(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\geckodriver.exe")
-browser.get('https://anoeye.com/login')
+browser.get('abc')
 
-browser.find_element_by_name('email').send_keys("engrfarkhanda6@gmail.com")
-browser.find_element_by_name('password').send_keys("haneen32303")
+browser.find_element_by_name('email').send_keys("xyz")
+browser.find_element_by_name('password').send_keys("pak1234")
 browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[1]/div/div[2]/div[2]/form/div[5]/button').click()
 print("Logged in Successfully")
 browser.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div[2]/div[4]/div[1]/div/span[2]").click()
@@ -16,7 +16,7 @@ browser.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div[2]/div[4]/di
 time.sleep(2)
 print("profile page appears")
 browser.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[2]/div[2]/a[2]/span/span[2]").click()
-browser.find_element_by_name("old_password").send_keys("haneen32303")
+browser.find_element_by_name("old_password").send_keys("32303")
 browser.find_element_by_name("password").send_keys("pak1234")
 browser.find_element_by_name("confirm_password").send_keys("pak1234")
 browser.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[2]/div/div[2]/div/div[2]/div/div/div/form/div[2]/div/div/div[2]/button[1]").click()
