0def357c9728c9f4a19a0381f6bfcc2030f47b0d
farkhanda-rasheed <farkhanda.wasay@yahoo.com>
2021-06-15 14:28:24 +0500

    Website is opened in multi browsers

--- MultiBrowser.py
+++ MultiBrowser.py
@@ -5,4 +5,4 @@ from selenium.webdriver.common.keys import Keys
 driver=webdriver.Firefox(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\geckodriver.exe")
 #driver=webdriver.Ie(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\IEDriverServer.exe")
 
-driver.get("https://anoeye.com/login")
+driver.get("abc")
