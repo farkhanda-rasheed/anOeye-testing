bd125196a244d0d0f12e553dad746cd39f00bfca
farkhanda-rasheed <farkhanda.wasay@yahoo.com>
2021-06-15 14:17:18 +0500

    URL chnaged

--- Login.py
+++ Login.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 
 driver=webdriver.Chrome(executable_path="C:\\Users\\shehwar\\PycharmProjects\\SeleniumProject\\Drivers\\chromedriver.exe")
 
-driver.get("https://anoeye.com/login")
-driver.find_element_by_name("email").send_keys("engrfarkhanda6@gmail.com")
-driver.find_element_by_name("password").send_keys("haneen32303")
+driver.get("abc")
+driver.find_element_by_name("email").send_keys("xyz")
+driver.find_element_by_name("password").send_keys("pak1234")
 driver.find_element_by_xpath("//*[@id='kt_login']/div/div[2]/div/div[1]/div/div[2]/div[2]/form/div[5]/button").click()
